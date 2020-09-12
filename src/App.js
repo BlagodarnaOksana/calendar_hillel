d875d792abbf556e2164a786a91e396ae12fe843
BlagodarnaOksana <blagodarnaoksana@ukr.net>
2020-09-12 20:06:17 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './App.css';
 import Year from './components/year/year';
 import Navbar from './components/navbar/navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   
   return (
-    <div className='App'>
+    <Router>
       <Navbar />
-      <Year />
       
-    </div>
+      <Route path="/year" exact>
+        <Year />
+      </Route>
+      
+    </Router>
   );
 }
 
